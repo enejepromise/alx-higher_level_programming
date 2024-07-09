cbe6abbcd56ae170e3680cdcf01ad516899b4f51
eneje promise <enejepromise@gmail.com>
2024-07-09 15:29:45 -0700

    update

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a DELETE request and displays the body from a get request of a specified url
-curl -X DELETE -s "$1"i
+curl -X DELETE -s "$1"
--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Ascript that sends a GET request to the specified URL, and displays the body of the response
-curl -H "X-School-User-Id: 98" -s "$1"i
+curl -H "X-School-User-Id: 98" -s "$1"
